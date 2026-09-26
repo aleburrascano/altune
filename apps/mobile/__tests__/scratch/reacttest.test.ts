ce24f28855c024103ec37568d9129b27a8fc13cd
aleburrascano <158299836+aleburrascano@users.noreply.github.com>
2026-09-25 23:04:20 -0400

    chore(mobile): drop a diagnostic scratch test (#2833)

--- apps/mobile/__tests__/scratch/reacttest.test.ts
+++
@@ -1,8 +0,0 @@
-const outer = require('react');
-test('same react instance inside isolateModules', () => {
-  let inner: unknown;
-  jest.isolateModules(() => {
-    inner = require('react');
-  });
-  expect(inner).toBe(outer);
-});
